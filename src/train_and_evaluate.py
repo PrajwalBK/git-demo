1695d317f6f64b25d225bb838d3a831081edaf18
PrajwalBK <prajwalbkhavatkopp@gmail.com>
2024-07-11 16:29:17 +0530

    dvc data

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -11,7 +11,13 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 import json
 import mlflow
-import urllib.parse import urllib
+#import urllib.parse import urllib
+
+def eval_metrics(actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual,pred))
+    mae =mean_absolute_error(actual,pred)
+    r2 = r2_score(actual,pred)
+    return rmse,mae,r2
 
 
 def train_and_evaluate(config_path):
@@ -47,11 +53,13 @@ def train_and_evaluate(config_path):
     (rmse, mae, r2) = eval_metrics(test_y, predicted_value)
 
     print("Elastic Model (alpha= %f, l1_ratio= %f):" %(alpha, l1_ratio))
+    print("RMSC :%s" %rmse)
+    print("MAE :%s" %mae)
+    print("R2 :%s" %r2)
 
-
-
-
-
+    os.makedirs(model_dir,exist_ok=True)
+    model_path = os.path.join(model_dir,"model.joblib")
+    joblib.dump(lr,model_path)
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
