ddb96ca429666097ab1789744717c29c3508e376
PrajwalBK <prajwalbkhavatkopp@gmail.com>
2024-07-11 15:55:46 +0530

    train data

--- src/split_data.py
+++ src/split_data.py
@@ -24,11 +24,7 @@ def split_data(config_path):
     test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
 
 
-
-
-
-
-if __name__=="__main___":
+if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
